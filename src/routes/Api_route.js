2ef57aaa89985ec4e1fb95556b10fd43128b690f
Ajay krishnan creations <118874449+AjayKrishnanKen42@users.noreply.github.com>
2023-11-28 10:19:55 +0530

    google storage

--- src/routes/Api_route.js
+++ src/routes/Api_route.js
@@ -1,10 +1,10 @@
-const {Router} = require("express");
-const express = require('express');
-const {register,login,course,getCourse,updateUser,getAllStudentInfo,getVideos,addNewVideo,getUserDetais,addNewPost,getPosts,courseUpdate,RefreshToken} = require("../services/Api");
+const { Router } = require("express");
+const express = require("express");
+const { register, login, course, getCourse, updateUser, getAllStudentInfo, getVideos, addNewVideo, getUserDetais, addNewPost, getPosts, courseUpdate, RefreshToken } = require("../services/Api");
 const router = Router();
 const validateToken = require("../helpers/validateTokenHandler");
 // const multer = require('multer');
-const fs = require('fs');
+const fs = require("fs");
 const path = require("path");
 const { Storage } = require("@google-cloud/storage");
 const Multer = require("multer");
@@ -40,49 +40,31 @@ router.get("/stream/:videoFileName", async (req, res) => {
   try {
     const videoFileName = req.params.videoFileName;
     const file = bucket.file(videoFileName);
-
-    // Get the file size
     const [metadata] = await file.getMetadata();
-    const fileSize = (metadata?.size) || undefined;
-
+    const fileSize = metadata?.size || undefined;
     if (fileSize !== undefined) {
-      // Set the content type
       res.setHeader("Content-Type", "video/mp4");
-
-      // Parse Range header to get the start and end bytes
       const range = req.headers.range;
-
       if (range) {
         const parts = range.replace(/bytes=/, "").split("-");
         const start = parseInt(parts[0], 10);
         const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
-
-        // Calculate chunk size (you can adjust this value)
         const chunkSize = 10 ** 6; // 1 MB
-
-        // Set the response headers for partial content
         res.setHeader("Content-Range", `bytes ${start}-${end}/${fileSize}`);
         res.setHeader("Accept-Ranges", "bytes");
         res.setHeader("Content-Length", chunkSize);
         res.status(206);
-
-        // Stream the file in chunks
         const readStream = file.createReadStream({ start, end });
-
         readStream.on("error", (err) => {
           console.error("Error reading stream:", err);
           res.status(500).send("Internal Server Error");
         });
-
         readStream.pipe(res);
-
         console.log("Success");
       } else {
-        // Handle the case when 'range' is undefined
         res.status(400).send("Bad Request: Range header is missing");
       }
     } else {
-      // Handle the case when 'fileSize' is undefined
       res.status(500).send("Internal Server Error: File size is undefined");
     }
   } catch (error) {
@@ -91,24 +73,23 @@ router.get("/stream/:videoFileName", async (req, res) => {
   }
 });
 
+router.post("/register", register);
 
-router.post("/register",register);
+router.post("/login", login);
 
-router.post("/login",login);
+router.post("/addcourse", validateToken, course);
+router.get("/getcourse", validateToken, getCourse);
+router.put("/updateUser", validateToken, updateUser);
 
-router.post("/addcourse",validateToken,course)
-router.get('/getcourse',validateToken,getCourse);
-router.put('/updateUser',validateToken,updateUser);
+router.get("/getAllStudentInfo", validateToken, getAllStudentInfo);
+router.get("/getUserDetais/:email", validateToken, getUserDetais);
 
-router.get('/getAllStudentInfo',validateToken,getAllStudentInfo) ;
-router.get('/getUserDetais/:email',validateToken,getUserDetais);
+router.post("/addNewPost", validateToken, addNewPost);
+router.get("/getPosts", validateToken, getPosts);
 
-router.post("/addNewPost",validateToken,addNewPost)
-router.get('/getPosts',validateToken,getPosts);
+router.put("/courseUpdate", validateToken, courseUpdate);
 
-router.put('/courseUpdate',validateToken,courseUpdate);
-
-router.post("/RefreshToken",RefreshToken);
+router.post("/RefreshToken", RefreshToken);
 
 // const storage = multer.diskStorage({
 //     destination: (req, file, cb) => {
@@ -118,7 +99,7 @@ router.post("/RefreshToken",RefreshToken);
 //       cb(null, file.originalname);
 //     },
 //   });
-//   const upload = multer({ storage });  
+//   const upload = multer({ storage });
 // router.post('/addNewVideo', upload.single('video'), async (req, res) => {
 //     try {
 //       const { title } = req.body;
@@ -132,27 +113,27 @@ router.post("/RefreshToken",RefreshToken);
 //     }
 //   });
 
-  // router.get("/:name", function (req, res) {
-  //   const range = req.headers.range;
-  //   if (!range) {
-  //     res.status(400).send("Requires Range header");
-  //   }
-  //   const videoPath = "./uploads/"+req.params.name;
-  //   const videoSize = fs.statSync("./uploads/"+req.params.name).size;
-  //   const CHUNK_SIZE =  300*1000; // 300KB
-  //   const start = Number(range.replace(/\D/g, ""));
-  //   const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
-  //   const contentLength = end - start + 1;
-  //   const headers = {
-  //     "Content-Range": `bytes ${start}-${end}/${videoSize}`,
-  //     "Accept-Ranges": "bytes",
-  //     "Content-Length": contentLength,
-  //     "Content-Type": "video/mp4",
-  //   };
-  //   res.writeHead(206, headers);
-  //   const videoStream = fs.createReadStream(videoPath, { start, end });
-  //   videoStream.pipe(res);
-  // });
+// router.get("/:name", function (req, res) {
+//   const range = req.headers.range;
+//   if (!range) {
+//     res.status(400).send("Requires Range header");
+//   }
+//   const videoPath = "./uploads/"+req.params.name;
+//   const videoSize = fs.statSync("./uploads/"+req.params.name).size;
+//   const CHUNK_SIZE =  300*1000; // 300KB
+//   const start = Number(range.replace(/\D/g, ""));
+//   const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+//   const contentLength = end - start + 1;
+//   const headers = {
+//     "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+//     "Accept-Ranges": "bytes",
+//     "Content-Length": contentLength,
+//     "Content-Type": "video/mp4",
+//   };
+//   res.writeHead(206, headers);
+//   const videoStream = fs.createReadStream(videoPath, { start, end });
+//   videoStream.pipe(res);
+// });
 
-  router.get('/getVideos/video',getVideos);
+router.get("/getVideos/video", getVideos);
 module.exports = router;
